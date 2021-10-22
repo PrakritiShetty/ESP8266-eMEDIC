1ea8498c7feaab1e0ba267f365d0a54da748226d
Abh300 <40433190+Abh300@users.noreply.github.com>
2021-10-22 12:44:23 +0530

    Add files via upload
    Final Code for Pitch Presentation at eMEDIC event

--- stepper_speedControl_modified.ino
+++ stepper_speedControl_modified.ino
@@ -18,98 +18,163 @@
  */
 
 #include <Stepper.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
 
+
+// Replace with your network credentials
+const char* ssid = "LAPTOP-M2CKJ6L2 9419";
+const char* password = "1)j8H336";
+
+#define DHTPIN 27     // Digital pin connected to the DHT sensor
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
 // for your motor
-
-// Rotary Encoder Inputs
-#define CLK 5
-#define DT 4
-#define SW 15
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 13, 14, 12, 27);
 
-
-int counter = 0;
-int currentStateCLK;
-int lastStateCLK;
-String currentDir ="";
-unsigned long lastButtonPress = 0;
-
 int stepCount = 0;  // number of steps the motor has taken
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
-void setup() {
-  // nothing to do inside the setup
-  
-  // Set encoder pins as inputs
-  pinMode(CLK,INPUT);
-  pinMode(DT,INPUT);
-  pinMode(SW, INPUT_PULLUP);
+//DHT dht(DHTPIN, DHTTYPE);
 
-  // Setup Serial Monitor
-  Serial.begin(115200);
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
-  // Read the initial state of CLK
-  lastStateCLK = digitalRead(CLK);
-
+String readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  int motorSpeed = map(analogRead(34), 0,4095, 15,20);
+  float t = motorSpeed;
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
 }
 
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP32 Distraction Monitoring Server</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Temperature</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">&percnt;</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 2000 ) ;
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return readDHTTemperature();
+  }
+//  else if(var == "HUMIDITY"){
+//    return readDHTHumidity();
+//  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+
+
+  // Start server
+  server.begin();
+}
+ 
+
+
+
+
 void loop() {
-  
-  // Read the current state of CLK
-  currentStateCLK = digitalRead(CLK);
-
-  // If last and current state of CLK are different, then pulse occurred
-  // React to only 1 state change to avoid double count
-  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-  
-    // If the DT state is different than the CLK state then
-    // the encoder is rotating CCW so decrement
-    if (digitalRead(DT) != currentStateCLK) {
-      counter --;
-      currentDir ="CCW";
-    } else {
-      // Encoder is rotating CW so increment
-      counter ++;
-      currentDir ="CW";
-    }
-
-    Serial.print("Direction: ");
-    Serial.print(currentDir);
-    Serial.print(" | Counter: ");
-    Serial.println(counter);
-    delay(100);
-  }
-
-  
-  // Remember last CLK state
-  lastStateCLK = currentStateCLK;
-
-  // Read the button state
-  int btnState = digitalRead(SW);
-
-  //If we detect LOW signal, button is pressed
-  if (btnState == LOW) {
-    //if 50ms have passed since last LOW pulse, it means that the
-    //button has been pressed, released and pressed again
-    if (millis() - lastButtonPress > 50) {
-      Serial.println("Button pressed!");
-    }
-
-    // Remember last button press event
-    lastButtonPress = millis();
-  }
-
-  // Put in a slight delay to help debounce the reading
-  delay(1);
-  
-  
-  // read the sensor value:
-  
-  int sensorReading = counter;
+    
+  int sensorReading = analogRead(34);
   // map it to a range from 0 to 100:
-  int motorSpeed = map(sensorReading, -10,10, 6,10);
+  int motorSpeed = map(sensorReading, 0,4095, 15,20);
+  Serial.println(motorSpeed);
+  delay(100);
   // set the motor speed:
   if (motorSpeed > 0) {
     myStepper.setSpeed(motorSpeed);
@@ -117,5 +182,5 @@ void loop() {
     myStepper.step(stepsPerRevolution / 100);
   }  
     
-  
+
 }
