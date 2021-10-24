3e2a5c652ca17a3096f79572cd203375f0b80740
Abh300 <40433190+Abh300@users.noreply.github.com>
2021-10-24 08:49:30 +0530

    Add files via upload
    Code for control of distraction rate thru pot

---
+++ stepper_speedControl_modified_length_new.ino
@@ -0,0 +1,184 @@
+
+/*
+ Stepper Motor Control - speed control
+
+ This program drives a unipolar or bipolar stepper motor.
+ The motor is attached to digital pins 8 - 11 of the Arduino.
+ A potentiometer is connected to analog input 0.
+
+ The motor will rotate in a clockwise direction. The higher the potentiometer value,
+ the faster the motor speed. Because setSpeed() sets the delay between steps,
+ you may notice the motor is less responsive to changes in the sensor value at
+ low speeds.
+
+ Created 30 Nov. 2009
+ Modified 28 Oct 2010
+ by Tom Igoe
+
+ */
+
+#include <Stepper.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+
+
+// Replace with your network credentials
+const char* ssid = "ashok";
+const char* password = "9321411306";
+
+#define DHTPIN 27     // Digital pin connected to the DHT sensor
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+// for your motor
+
+float temp = 0.0;
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 13, 14, 12, 27);
+
+int stepCount = 0;  // number of steps the motor has taken
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+//DHT dht(DHTPIN, DHTTYPE);
+
+float dist_length = 0;
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readDHTTemperature() {
+
+  int motorSpeed = map(analogRead(34), 0,4095, 0,20);
+  float realSpeed = 1;
+  float t = dist_length;
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    dist_length= dist_length + 2.8;
+    return String(t);
+  }
+  
+}
+
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP32 Distraction Monitoring Server</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Distraction Length Travelled</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">mm</sup>
+  </p>
+
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+//setInterval(function ( ) {
+//  var xhttp = new XMLHttpRequest();
+//  xhttp.onreadystatechange = function() {
+//    if (this.readyState == 4 && this.status == 200) {
+//      document.getElementById("humidity").innerHTML = this.responseText;
+//    }
+//  };
+//  xhttp.open("GET", "/humidity", true);
+//  xhttp.send();
+//}, 2000 ) ;
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return readDHTTemperature();
+  }
+//  else if(var == "HUMIDITY"){
+//    return readDHTHumidity();
+//  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+
+ 
+  // Start server
+  server.begin();
+}
+ 
+
+
+
+
+void loop() {
+    
+  int sensorReading = analogRead(34);
+  // map it to a range from 0 to 100:
+  int motorSpeed = map(sensorReading, 0,4095, 0,20);
+  Serial.println(motorSpeed);
+  delay(100);
+  // set the motor speed:
+  if (motorSpeed > 0) {
+    myStepper.setSpeed(motorSpeed);
+    // step 1/100 of a revolution:
+    myStepper.step(stepsPerRevolution / 50);
+  }  
+    
+
+}
